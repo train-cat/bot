84cbf440402f7e1131b15f4cbf01365ad5dfe6fc
Eraac <kevin@labesse.me>
2017-12-09 14:57:59 +0100

    remove unused var

--- helper/const.go
+++ helper/const.go
@@ -10,8 +10,3 @@ const (
 	ExitCodeErrorStopServer
 	ExitCodeErrorInitNotification
 )
-
-var (
-	// Platforms we have to interact with this bot
-	Platforms = []string{dialogflow.PlatformDialogflow, dialogflow.PlatformTelegram, dialogflow.PlatformFacebook}
-)
