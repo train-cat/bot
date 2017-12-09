5d64222e8f072813e730aeca5a514c79243748c9
Eraac <kevin@labesse.me>
2017-12-09 14:39:53 +0100

    add cache for station name

--- api/find.go
+++ api/find.go
@@ -2,14 +2,24 @@ package api
 
 import "github.com/train-cat/client-train-go"
 
+var cacheStationName = map[int]string {}
+
 // FindStationNameByID return station name
 func FindStationNameByID(stationID int) string {
+	name, exist := cacheStationName[stationID]
+
+	if exist && name != "" {
+		return name
+	}
+
 	s, err := traincat.GetStation(stationID)
 
 	if err != nil {
 		// TODO log
 		return ""
 	}
+
+	cacheStationName[stationID] = s.Name
 
 	return s.Name
 }
